8b159feb4fdeeee7c6b4a4f8df26de7d4e0aba9e
Renan Oronfle <renanoronfle@gmail.com>
2017-01-31 23:49:33 -0200

    add array lib with sum method

--- lib/saphira.rb
+++ lib/saphira.rb
@@ -1,8 +1,10 @@
 require "saphira/version"
+require "saphira/array"
 
 module Saphira
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [200, {'Content-Type' =>  'text/html'},
        ["Hello From Saphira Web Framework"]]
     end
