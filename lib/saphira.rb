fa01717f19d607b47b8714ce56ec80093a3daf6e
Renan Oronfle <renanoronfle@gmail.com>
2017-02-06 19:33:16 -0200

    first routing version

--- lib/saphira.rb
+++ lib/saphira.rb
@@ -1,12 +1,32 @@
 require "saphira/version"
 require "saphira/array"
+require "saphira/routing"
 
 module Saphira
   class Application
     def call(env)
+
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+
       `echo debug > debug.txt`;
-      [200, {'Content-Type' =>  'text/html'},
-       ["Hello From Saphira Web Framework"]]
+      [200, {'Content-Type' => 'text/html'},
+        [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
